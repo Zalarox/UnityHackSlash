fe5fd128b5bc54bb0b1b78a397083486fdf6c26e
Siddhant Bansal <bansal.siddhant@gmail.com>
2015-12-25 22:07:48 +0530

    First Commit

---
+++ Scripts/HandleTap.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HandleTap : MonoBehaviour {
+
+    public GameObject OptionsPanel;
+
+    public void StartGame()
+    {
+        // read progress and start the game
+        
+    }
+
+    public void ShowOptions()
+    {
+        // show the options menu
+        OptionsPanel.SetActive(true);
+    }
+
+    public void CloseOptions()
+    {
+        
+    }
+}
---
+++ Scripts/move.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class move : MonoBehaviour {
+
+    public float moveSpeed = 5f;
+    Vector3 movementInput;
+
+	void Update () {
+        movementInput = new Vector3(CrossPlatformInputManager.GetAxis("Horizontal"), 0, CrossPlatformInputManager.GetAxis("Vertical")) * Time.deltaTime;
+        transform.Translate(movementInput * moveSpeed);
+	}
+}
